fbde41d346fd96ad98ae41b7ed27efc1235fd067
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-03-16 22:14:23 +1100

    Create RelayCommand.cs

---
+++ SyncNotesAppTest_Windows/Commands/RelayCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SyncNotesAppTest_Windows.Commands
+{
+
+    public class RelayCommand : ICommand
+    {
+
+        #region - - - - - Fields - - - - -
+
+        private Action<object> ExecuteAction { get; set; }
+
+        private Predicate<object> CanExecutePredicate { get; set; }
+
+        #endregion
+
+        #region - - - - - Constructors - - - - -
+
+        public RelayCommand(Action<object> executeAction, Predicate<object> canExecutePredicate)
+        {
+            this.ExecuteAction = executeAction ?? throw new ArgumentNullException(nameof(executeAction));
+            this.CanExecutePredicate =
+                canExecutePredicate ?? throw new ArgumentNullException(nameof(canExecutePredicate));
+        }
+
+        # endregion Constructors
+
+        #region - - - - - Events - - - - -
+
+        public event EventHandler? CanExecuteChanged;
+
+        #endregion Events
+
+        #region - - - - - Methods - - - - -
+
+        public bool CanExecute(object? parameter)
+            => this.CanExecutePredicate.Invoke(parameter);
+
+        public void Execute(object? parameter)
+            => this.ExecuteAction.Invoke(parameter);
+
+        #endregion Methods
+
+    }
+
+}
