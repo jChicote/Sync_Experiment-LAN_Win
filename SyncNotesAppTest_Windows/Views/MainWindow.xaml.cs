c60a17ee1b8272c6a979684a251077c340f3bee5
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-03-17 00:32:57 +1100

    Update MainWindow.xaml.cs

--- SyncNotesAppTest_Windows/Views/MainWindow.xaml.cs
+++ SyncNotesAppTest_Windows/Views/MainWindow.xaml.cs
@@ -1,14 +1,6 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using SyncNotesAppTest_Windows.Application.Data;
 using SyncNotesAppTest_Windows.ViewModels;
+using System.Windows;
 
 namespace SyncNotesAppTest_Windows
 {
@@ -22,6 +14,7 @@ namespace SyncNotesAppTest_Windows
             InitializeComponent();
 
             this.DataContext = new MainViewModel();
+            _ = new ApplicationDbContext();
         }
     }
 }
